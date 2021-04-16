6faefba4c42e02a219cd108f5e80a9853747f495
Yoann Marguerite <yoann.marguerite@orange.fr>
2021-04-16 15:17:56 +0200

    ignore pg-native

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Note: we provide webpack above so you should not `require` it
+      // Perform customizations to webpack config
+      config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/))
+  
+      // Important: return the modified config
+      return config
+    },
+  }
